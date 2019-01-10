20ac197b72cf81b5ecc7ef84c39e52770ff16b36
felsabbagh3 <fsabbagh@gatech.edu>
2019-01-10 15:58:11 -0500

    minor config change

--- src/config.h
+++ src/config.h
@@ -1,7 +1,9 @@
 #ifndef __OPTIONS__
 #define __OPTIONS__
+#define ICACHE_ENABLE
 #define ICACHE_BITS 15
 #define ILINE_BITS 10
+#define DCACHE_ENABLE
 #define DCACHE_BITS 15
 #define DLINE_BITS 10
 #define JAL_MEM
