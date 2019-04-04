d5d30f0c415d2b4486eaac2e29e631bb3d43d13d
felsabbagh3 <fsabbagh@gatech.edu>
2019-04-03 21:41:09 -0400

    Added benchmarking

--- src/Sphinx.cpp
+++ src/Sphinx.cpp
@@ -8,7 +8,8 @@
 #include "parse.h"
 
 
-#define NUM_TESTS 39
+// #define NUM_TESTS 39
+#define NUM_TESTS 7
 
 int main(int argc, char ** argv)
 {
@@ -19,75 +20,97 @@ int main(int argc, char ** argv)
     Sphinx sphinx;
 
     bool passed = true;
-	std::string tests[NUM_TESTS] = {
-		"../tests/rv32ui-p-add.hex",
-		"../tests/rv32ui-p-addi.hex",
-		"../tests/rv32ui-p-and.hex",
-		"../tests/rv32ui-p-andi.hex",
-		"../tests/rv32ui-p-auipc.hex",
-		"../tests/rv32ui-p-beq.hex",
-		"../tests/rv32ui-p-bge.hex",
-		"../tests/rv32ui-p-bgeu.hex",
-		"../tests/rv32ui-p-blt.hex",
-		"../tests/rv32ui-p-bltu.hex",
-		"../tests/rv32ui-p-bne.hex",
-		"../tests/rv32ui-p-jal.hex",
-		"../tests/rv32ui-p-jalr.hex",
-		"../tests/rv32ui-p-lb.hex",
-		"../tests/rv32ui-p-lbu.hex",
-		"../tests/rv32ui-p-lh.hex",
-		"../tests/rv32ui-p-lhu.hex",
-		"../tests/rv32ui-p-lui.hex",
-		"../tests/rv32ui-p-lui.hex.hex",
-		"../tests/rv32ui-p-lw.hex",
-		"../tests/rv32ui-p-or.hex",
-		"../tests/rv32ui-p-ori.hex",
-		"../tests/rv32ui-p-sb.hex",
-		"../tests/rv32ui-p-sh.hex",
-		"../tests/rv32ui-p-simple.hex",
-		"../tests/rv32ui-p-sll.hex",
-		"../tests/rv32ui-p-slli.hex",
-		"../tests/rv32ui-p-slt.hex",
-		"../tests/rv32ui-p-slti.hex",
-		"../tests/rv32ui-p-sltiu.hex",
-		"../tests/rv32ui-p-sltu.hex",
-		"../tests/rv32ui-p-sra.hex",
-		"../tests/rv32ui-p-srai.hex",
-		"../tests/rv32ui-p-srl.hex",
-		"../tests/rv32ui-p-srli.hex",
-		"../tests/rv32ui-p-sub.hex",
-		"../tests/rv32ui-p-sw.hex",
-		"../tests/rv32ui-p-xor.hex",
-		"../tests/rv32ui-p-xori.hex",
-		// "../tests/I-CSRRC-01.elf.hex",
-		// "../tests/I-CSRRCI-01.elf.hex",
-		// "../tests/I-CSRRS-01.elf.hex",
-		// "../tests/I-CSRRSI-01.elf.hex",
-		// "../tests/I-CSRRW-01.elf.hex",
-		// "../tests/I-CSRRWI-01.elf.hex",
-  //       "../tests/I-DELAY_SLOTS-01.elf.hex",
-  //       "../tests/I-EBREAK-01.elf.hex",
-  //       "../tests/I-ECALL-01.elf.hex",
-  //       "../tests/I-ENDIANESS-01.elf.hex"
-		// "tests/dhrystoneO3.hex","
+	// std::string tests[NUM_TESTS] = {
+	// 	"../tests/rv32ui-p-add.hex",
+	// 	"../tests/rv32ui-p-addi.hex",
+	// 	"../tests/rv32ui-p-and.hex",
+	// 	"../tests/rv32ui-p-andi.hex",
+	// 	"../tests/rv32ui-p-auipc.hex",
+	// 	"../tests/rv32ui-p-beq.hex",
+	// 	"../tests/rv32ui-p-bge.hex",
+	// 	"../tests/rv32ui-p-bgeu.hex",
+	// 	"../tests/rv32ui-p-blt.hex",
+	// 	"../tests/rv32ui-p-bltu.hex",
+	// 	"../tests/rv32ui-p-bne.hex",
+	// 	"../tests/rv32ui-p-jal.hex",
+	// 	"../tests/rv32ui-p-jalr.hex",
+	// 	"../tests/rv32ui-p-lb.hex",
+	// 	"../tests/rv32ui-p-lbu.hex",
+	// 	"../tests/rv32ui-p-lh.hex",
+	// 	"../tests/rv32ui-p-lhu.hex",
+	// 	"../tests/rv32ui-p-lui.hex",
+	// 	"../tests/rv32ui-p-lui.hex.hex",
+	// 	"../tests/rv32ui-p-lw.hex",
+	// 	"../tests/rv32ui-p-or.hex",
+	// 	"../tests/rv32ui-p-ori.hex",
+	// 	"../tests/rv32ui-p-sb.hex",
+	// 	"../tests/rv32ui-p-sh.hex",
+	// 	"../tests/rv32ui-p-simple.hex",
+	// 	"../tests/rv32ui-p-sll.hex",
+	// 	"../tests/rv32ui-p-slli.hex",
+	// 	"../tests/rv32ui-p-slt.hex",
+	// 	"../tests/rv32ui-p-slti.hex",
+	// 	"../tests/rv32ui-p-sltiu.hex",
+	// 	"../tests/rv32ui-p-sltu.hex",
+	// 	"../tests/rv32ui-p-sra.hex",
+	// 	"../tests/rv32ui-p-srai.hex",
+	// 	"../tests/rv32ui-p-srl.hex",
+	// 	"../tests/rv32ui-p-srli.hex",
+	// 	"../tests/rv32ui-p-sub.hex",
+	// 	"../tests/rv32ui-p-sw.hex",
+	// 	"../tests/rv32ui-p-xor.hex",
+	// 	"../tests/rv32ui-p-xori.hex",
+	// 	// "../tests/I-CSRRC-01.elf.hex",
+	// 	// "../tests/I-CSRRCI-01.elf.hex",
+	// 	// "../tests/I-CSRRS-01.elf.hex",
+	// 	// "../tests/I-CSRRSI-01.elf.hex",
+	// 	// "../tests/I-CSRRW-01.elf.hex",
+	// 	// "../tests/I-CSRRWI-01.elf.hex",
+ //  //       "../tests/I-DELAY_SLOTS-01.elf.hex",
+ //  //       "../tests/I-EBREAK-01.elf.hex",
+ //  //       "../tests/I-ECALL-01.elf.hex",
+ //  //       "../tests/I-ENDIANESS-01.elf.hex"
+	// 	// "tests/dhrystoneO3.hex","
+	// };
+
+	std::string bench[NUM_TESTS] = {
+		"../riscv-benchmarks/dhrystone.riscv.hex",
+		"../riscv-benchmarks/vvadd.riscv.hex",
+		"../riscv-benchmarks/towers.riscv.hex",
+		"../riscv-benchmarks/spmv.riscv.hex",
+		"../riscv-benchmarks/median.riscv.hex",
+		"../riscv-benchmarks/qsort.riscv.hex",
+		"../riscv-benchmarks/multiply.riscv.hex"
 	};
+
 
 	if (es.state == 0)
 	{
+
 		for (int ii = 0; ii < NUM_TESTS; ii++)
 		// for (int ii = 0; ii < NUM_TESTS - 1; ii++)
 		{
-			bool curr = sphinx.simulate(tests[ii]);
+			bool curr = sphinx.simulate(bench[ii]);
 
-			if ( curr) std::cout << GREEN << "Test Passed: " << tests[ii] << std::endl;
-			if (!curr) std::cout << RED   << "Test Failed: " << tests[ii] << std::endl;
-			passed = passed && curr;
+			std::cout << GREEN << "Benchmarked: " << bench[ii] << std::endl;
 
 			std::cout << DEFAULT;
 		}
 
-		if( passed) std::cout << DEFAULT << "PASSED ALL TESTS\n";
-		if(!passed) std::cout << DEFAULT << "Failed one or more tests\n";
+		// for (int ii = 0; ii < NUM_TESTS; ii++)
+		// // for (int ii = 0; ii < NUM_TESTS - 1; ii++)
+		// {
+		// 	bool curr = sphinx.simulate(tests[ii]);
+
+		// 	if ( curr) std::cout << GREEN << "Test Passed: " << tests[ii] << std::endl;
+		// 	if (!curr) std::cout << RED   << "Test Failed: " << tests[ii] << std::endl;
+		// 	passed = passed && curr;
+
+		// 	std::cout << DEFAULT;
+		// }
+
+		// if( passed) std::cout << DEFAULT << "PASSED ALL TESTS\n";
+		// if(!passed) std::cout << DEFAULT << "Failed one or more tests\n";
 	}
 	else if (es.state == 1)
 	{
